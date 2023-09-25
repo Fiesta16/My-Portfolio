8a465fa98a7d8d2854711ff3e8c10a9612fb67fd
Aayush <87488247+Fiesta16@users.noreply.github.com>
2023-09-25 20:16:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var tablinks =document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+var sdmenu=document.getElementById("sidemenu");
+
+function openmenu(){
+    sdmenu.style.right='0';
+}
+function closemenu(){
+    sdmenu.style.right='-200px';
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzj7yNvjIoHbwq8nuIZIOeK54hKmeLkmNLgdEK3s-GsdrvWh9DZYQ6xJTYapQt6H1bk6w/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
